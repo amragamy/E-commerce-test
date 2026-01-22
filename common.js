6bbef9019cea67c9ce2a3231761da0e897be59f4
amragamy <amragamy09@gmail.com>
2026-01-23 00:35:28 +0200

    add About And Contact page

--- common.js
+++ common.js
@@ -1,17 +1,51 @@
-// Navbar Links
-// let links = document.querySelectorAll(".link");
-// links = Array.from(links);
+// Features
+const featuresData = [
+   {
+      id: 1,
+      img_path: "../img/features/f1.png",
+      desc: "Free Shipping"
+   },
+   {
+      id: 2,
+      img_path: "../img/features/f2.png",
+      desc: "Online Order"
+   },
+   {
+      id: 3,
+      img_path: "../img/features/f3.png",
+      desc: "Save Money"
+   },
+   {
+      id: 4,
+      img_path: "../img/features/f4.png",
+      desc: "Promotions"
+   },
+   {
+      id: 5,
+      img_path: "../img/features/f5.png",
+      desc: "Happy Sell"
+   },
+   {
+      id: 6,
+      img_path: "../img/features/f6.png",
+      desc: "F24/7 Support"
+   },
 
-// let active = document.querySelector('.active')
+]
 
-// links.forEach((ele)=>{
-//    ele.addEventListener('click', ()=>{
-//     ele.classList.add('active');
-//     active.classList.remove('active');
-//     active = ele;
-//    });
-// });
-
+let featuresParent = document.getElementById('features-parent');
+if(featuresParent){
+   let featuresChilds = "";
+   for(let i = 0 ; i < featuresData.length ; ++i){
+      featuresChilds += `
+         <div class="features-child" id=${featuresData[i].id}>
+            <div class="image"><img src=${featuresData[i].img_path} alt="img"></div>
+            <div class="desc"><span>${featuresData[i].desc}</span></div>
+         </div>
+      `
+   }
+   featuresParent.innerHTML = featuresChilds;
+}
 
 
 // menu & close of Navbar
@@ -26,7 +60,6 @@ menu.addEventListener('click', function (){
 close.addEventListener('click', function(){
    linksSmallScreen.classList.remove('appear-links');
 });
-
 
 
 // products & new-arrival-products
@@ -80,8 +113,13 @@ function displayData(arr, parent){
    parent.innerHTML = productsCards;
 }
 
-displayData(productsImage, Products);
-displayData(newArrivalProductsImage, newArrivalProducts);
+if(Products){
+  displayData(productsImage, Products);
+}
+
+if(newArrivalProducts){
+  displayData(newArrivalProductsImage, newArrivalProducts);
+}
 
 
 
